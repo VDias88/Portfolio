aba5aaa0d966420d8e0267e3398368fa40bc416a
Vinicius Dias <59458030+VDias88@users.noreply.github.com>
2021-02-23 17:27:35 -0300

    Portfolio

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementsByClassName('sidenav')[0].style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementsByClassName('sidenav')[0].style.width = "0";
+  }
+
